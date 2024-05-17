9694eaf39901cfc81d1c92512df104cd6080c0e2
smadar <smadarshoshan@gmail.com>
2024-05-17 16:25:04 +0300

    added data exploration

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import pandas as pd
+import json
 
-biobank_path = '/home/ofeksh2@mta.ac.il/biobank/ukb672220.csv'
-fields = ['eid', '2966']
 
-df = pd.read_csv(biobank_path, usecols=fields, nrows=100)
+def get_people_with_disease(df, disease_code_pattern):
+    people_with_disease_df = df[df['41202-0.0'].str.contains(disease_code_pattern)]
+    people_with_disease_df = pd.concat([people_with_disease_df, df[df['41204-0.0'].str.contains(disease_code_pattern)]])
 
-print(df)
+    return people_with_disease_df
+
+
+with open ('/home/ofeksh2@mta.ac.il/config.json', 'r', encoding='utf-8') as f:
+    config = json.load(f)
+
+biobank_path = config['biobank_path']
+fields = config['fields']
+
+df = pd.read_csv(biobank_path, usecols=fields, nrows=1000)
+
+print(df.head())
+df = df.dropna()
+
+diabetes_pattern = r'E1[0-4]*'
+people_with_diabetes_df = get_people_with_disease(df, diabetes_pattern)
+
+pancreatic_cancer_pattern = r'c25*'
+people_with_pancreatic_cancer_df = get_people_with_disease(df, pancreatic_cancer_pattern)
+
+sample_group_df = pd.concat([people_with_diabetes_df, people_with_pancreatic_cancer_df])
+print(sample_group_df)
 
 
 
